f553378816608339c4ac2e0c6459c528af97d454
Chris McLain <chris@ed.link>
2021-08-26 17:21:00 -0500

    Update gitignore. Add graph api methods.

--- lib/graph.ts
+++ lib/graph.ts
@@ -1,5 +1,5 @@
 import axios, { AxiosInstance } from 'axios';
-import { Convert, District, School } from '../../types/gen/ts/edlink';
+import { Class, Agent, Session, Convert, District, Enrollment, Person, School, Section, Course } from '../../types/gen/ts/edlink';
 import { Filter } from './filter';
 
 export abstract class GraphAPI {
@@ -14,7 +14,7 @@ export abstract class GraphAPI {
         });
     }
 
-    private async *paginate(url: string, filter?: Filter, limit?: number) {
+    protected async *paginate<T>(url: string, filter?: Filter, limit?: number, formatter?: (raw: any) => T): AsyncGenerator<T> {
         let remaining = limit;
         let next = `${url}?$first=10000${filter ? `&$filter=${filter.toString()}` : ''}`;
 
@@ -46,7 +46,7 @@ export class GraphV2 extends GraphAPI {
     }
 
     async *listDistricts(): AsyncGenerator<District> {
-        // todo
+        return this.paginate<District>('/districts', undefined, undefined, (raw) => Convert.toDistrict(raw));
     }
 
     async fetchDistrict(district_id: string): Promise<District> {
@@ -54,10 +54,130 @@ export class GraphV2 extends GraphAPI {
     }
 
     async *listSchools(filter?: Filter): AsyncGenerator<School> {
-        //todo
+        return this.paginate<School>('/schools', filter, undefined, (raw) => Convert.toSchool(raw));
     }
 
     async fetchSchool(school_id: string): Promise<School> {
         return this.axios.get(`/schools/${school_id}`).then((res) => Convert.toSchool(res.data));
+    }
+
+    async *listClasses(filter?: Filter): AsyncGenerator<Class> {
+        return this.paginate<Class>('/classes', filter, undefined, (raw) => Convert.toClass(raw));
+    }
+
+    async fetchClass(class_id: string): Promise<Class> {
+        return this.axios.get(`/classes/${class_id}`).then((res) => Convert.toClass(res.data));
+    }
+
+    async *listClassSections(class_id: string, filter?: Filter): AsyncGenerator<Section> {
+        return this.paginate<Section>(`/classes/${class_id}/sections`, filter, undefined, (raw) => Convert.toSection(raw));
+    }
+
+    async *listClassEnrollments(class_id: string, filter?: Filter): AsyncGenerator<Enrollment> {
+        return this.paginate<Enrollment>(`/classes/${class_id}/enrollments`, filter, undefined, (raw) => Convert.toEnrollment(raw));
+    }
+
+    async *listClassPeople(class_id: string, filter?: Filter): AsyncGenerator<Person> {
+        return this.paginate<Person>(`/classes/${class_id}/people`, filter, undefined, (raw) => Convert.toPerson(raw));
+    }
+
+    async *listClassTeachers(class_id: string, filter?: Filter): AsyncGenerator<Person> {
+        return this.paginate<Person>(`/classes/${class_id}/teachers`, filter, undefined, (raw) => Convert.toPerson(raw));
+    }
+
+    async *listClassStudents(class_id: string, filter?: Filter): AsyncGenerator<Person> {
+        return this.paginate<Person>(`/classes/${class_id}/students`, filter, undefined, (raw) => Convert.toPerson(raw));
+    }
+
+    async *listSections(filter?: Filter): AsyncGenerator<Section> {
+        return this.paginate<Section>('/sections', filter, undefined, (raw) => Convert.toSection(raw));
+    }
+
+    async fetchSection(section_id: string): Promise<Section> {
+        return this.axios.get(`/sections/${section_id}`).then((res) => Convert.toSection(res.data));
+    }
+
+    async *listSectionEnrollments(section_id: string, filter?: Filter): AsyncGenerator<Enrollment> {
+        return this.paginate<Enrollment>(`/sections/${section_id}/enrollments`, filter, undefined, (raw) => Convert.toEnrollment(raw));
+    }
+
+    async *listSectionPeople(section_id: string, filter?: Filter): AsyncGenerator<Person> {
+        return this.paginate<Person>(`/sections/${section_id}/people`, filter, undefined, (raw) => Convert.toPerson(raw));
+    }
+
+    async *listSectionTeachers(section_id: string, filter?: Filter): AsyncGenerator<Person> {
+        return this.paginate<Person>(`/sections/${section_id}/teachers`, filter, undefined, (raw) => Convert.toPerson(raw));
+    }
+
+    async *listSectionStudents(section_id: string, filter?: Filter): AsyncGenerator<Person> {
+        return this.paginate<Person>(`/sections/${section_id}/students`, filter, undefined, (raw) => Convert.toPerson(raw));
+    }
+
+    async *listPeople(filter?: Filter): AsyncGenerator<Person> {
+        return this.paginate<Person>('/people', filter, undefined, (raw) => Convert.toPerson(raw));
+    }
+
+    async fetchPerson(person_id: string): Promise<Person> {
+        return this.axios.get(`/people/${person_id}`).then((res) => Convert.toPerson(res.data));
+    }
+
+    async *listPersonEnrollments(person_id: string, filter?: Filter): AsyncGenerator<Enrollment> {
+        return this.paginate<Enrollment>(`/people/${person_id}/enrollments`, filter, undefined, (raw) => Convert.toEnrollment(raw));
+    }
+
+    async *listPersonDistricts(person_id: string, filter?: Filter): AsyncGenerator<District> {
+        return this.paginate<District>(`/people/${person_id}/districts`, filter, undefined, (raw) => Convert.toDistrict(raw));
+    }
+
+    async *listPersonSchools(person_id: string, filter?: Filter): AsyncGenerator<School> {
+        return this.paginate<School>(`/people/${person_id}/schools`, filter, undefined, (raw) => Convert.toSchool(raw));
+    }
+
+    async *listPersonClasses(person_id: string, filter?: Filter): AsyncGenerator<Class> {
+        return this.paginate<Class>(`/people/${person_id}/classes`, filter, undefined, (raw) => Convert.toClass(raw));
+    }
+
+    async *listPersonSections(person_id: string, filter?: Filter): AsyncGenerator<Section> {
+        return this.paginate<Section>(`/people/${person_id}/sections`, filter, undefined, (raw) => Convert.toSection(raw));
+    }
+
+    async *listPersonAgents(person_id: string, filter?: Filter): AsyncGenerator<Agent> {
+        return this.paginate<Agent>(`/people/${person_id}/agents`, filter, undefined, (raw) => Convert.toAgent(raw));
+    }
+
+    async *listEnrollments(filter?: Filter): AsyncGenerator<Enrollment> {
+        return this.paginate<Enrollment>('/enrollments', filter, undefined, (raw) => Convert.toEnrollment(raw));
+    }
+
+    async fetchEnrollment(enrollment_id: string): Promise<Person> {
+        return this.axios.get(`/enrollments/${enrollment_id}`).then((res) => Convert.toEnrollment(res.data));
+    }
+
+    async *listCourses(filter?: Filter): AsyncGenerator<Course> {
+        return this.paginate<Course>('/courses', filter, undefined, (raw) => Convert.toCourse(raw));
+    }
+
+    async fetchCourse(course_id: string): Promise<Course> {
+        return this.axios.get(`/courses/${course_id}`).then((res) => Convert.toCourse(res.data));
+    }
+
+    async *listCourseClasses(course_id: string, filter?: Filter): AsyncGenerator<Class> {
+        return this.paginate<Class>(`/courses/${course_id}/classes`, filter, undefined, (raw) => Convert.toClass(raw));
+    }
+
+    async *listSessions(filter?: Filter): AsyncGenerator<Session> {
+        return this.paginate<Course>('/sessions', filter, undefined, (raw) => Convert.toSession(raw));
+    }
+
+    async fetchSession(session_id: string): Promise<Session> {
+        return this.axios.get(`/sessions/${session_id}`).then((res) => Convert.toSession(res.data));
+    }
+
+    async *listAgents(filter?: Filter): AsyncGenerator<Agent> {
+        return this.paginate<Course>('/agents', filter, undefined, (raw) => Convert.toAgent(raw));
+    }
+
+    async fetchAgent(agent_id: string): Promise<Agent> {
+        return this.axios.get(`/agents/${agent_id}`).then((res) => Convert.toAgent(res.data));
     }
 }
